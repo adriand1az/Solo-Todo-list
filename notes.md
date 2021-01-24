4c169dbb0c92b3934cbcabe5afc17d9446bfc392
Adrian <63692318+adriand1az@users.noreply.github.com>
2021-01-24 13:36:58 -0600

    Functionality
    Ability to add only one item to the list

--- notes.md
+++ notes.md
@@ -1,8 +1,5 @@
 # Todo Application
 
 - Items/completed need to be fixed to the top of that scroll bar and stays in the middle
-- Enter a task needs to be placed starting from the left
 - Enter task needs a button to submit to the right of it
 - Implementation of adding new item to list
-- when check marked click a line goes through its text and vice versa
-- 
--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const checkbox = document.querySelector('.text-box');
-const submit = document.querySelector('.submit-btn')
-
+const submit = document.querySelector('.submit-btn');
+const newItem = document.createElement('LI');
+const taskContainer = document.querySelector('.task-container')
 submit.addEventListener('click', function(e){
     e.preventDefault();
-    console.log(checkbox.value)
+    newItem.innerText = checkbox.value
+    taskContainer.appendChild(newItem)
 })
